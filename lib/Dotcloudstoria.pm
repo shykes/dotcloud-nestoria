fc585e905f7059bf2d9995f0624f6256f793b0b3
Alex Balhatchet <kaoru@slackwise.net>
2011-05-19 11:42:24 +0100

    homepage mostly set up
    next up is to write the logic for /search which will redirect to
    /$loc/property/$lt, which will do magical things

--- lib/Dotcloudstoria.pm
+++ lib/Dotcloudstoria.pm
@@ -1,10 +1,29 @@
 package Dotcloudstoria;
+
 use Dancer ':syntax';
+use Try::Tiny;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index';
+    template 'index.tt', {}, { layout => 'home' };
 };
+
+get '/search' => sub {
+    try {
+        my $url = parse_search();
+        redirect $url;
+    }
+    catch {
+        redirect 'index.tt', {}, { layout => 'home' };
+    }
+}
+
+
+
+
+sub parse_search {
+
+}
 
 true;
---
+++ public/javascripts/home.js
@@ -0,0 +1,9 @@
+$(function() {
+
+    $('#q').val("Enter a location...");
+    $('#q').click(function() {
+        $('#q').val("");
+    });
+
+
+});
